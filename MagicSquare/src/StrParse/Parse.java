896e7c8244d335f452142330f92462f3a3a1a233
koeunsi <koeunsi@naver.com>
2023-01-31 14:12:23 +0900

    왜 서버로 켜지는지 확인 좀,,,

--- MagicSquare/src/StrParse/Parse.java
+++ MagicSquare/src/StrParse/Parse.java
@@ -1,7 +1,23 @@
 package StrParse;
 
+import java.util.Scanner;
+
 public class Parse {
 	
-	public void Parse(){}
-
-}
+	public static void main(String[] args) {
+		
+		//int a = 0;	//종료 조건
+		
+		while(true){
+			System.out.println("출력할 숫자를 입력하세요");
+			
+			Scanner scanner = new Scanner(System.in);
+			
+			String strIn = scanner.next();
+			
+			System.out.print("입력한 숫자"+strIn);
+		}
+		
+	}	//main e
+		
+}	
--- MagicSquare/src/StrParse/Str.java
+++ MagicSquare/src/StrParse/Str.java
@@ -2,8 +2,4 @@ package StrParse;
 
 public class Str {
 
-	public static void main(String[] args) {
-		Parse parse = new Parse();
-		
-	}
 }
