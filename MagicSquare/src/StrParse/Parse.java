4c571fc6c923f10c2be6306034909082ff9a5503
koeunsi <koeunsi@naver.com>
2023-02-02 17:46:32 +0900

    문자열 파싱 완료

--- MagicSquare/src/StrParse/Parse.java
+++ MagicSquare/src/StrParse/Parse.java
@@ -3,28 +3,19 @@ package StrParse;
 import java.util.Scanner;
 
 public class Parse {
-	
-	public static void main(String[] args) {
-		
-		System.out.println("출력할 숫자를 입력하세요");
-		int a = 0;
-		while(true){
-			
-			Scanner scanner = new Scanner(System.in);
-			
-			String strIn = scanner.next();
-			
-			try {
-				System.out.println("입력한 수 : "+ strIn);
-				
-				a = Integer.parseInt(strIn);	//문자로 받은 값을 숫자로 파싱해주기
-				break; //0을 누르면 종료
-				
-			} catch (Exception e) {
-				//throw e;
-			}
-		}
-		//return a;
+
+		public static void main(String[] args) {
+		    Scanner scan = new Scanner(System.in);
+		    System.out.print("입력할 수 : ");
+		    String input = scan.nextLine();
+		    for (int i = 0; i < input.length(); i++) {
+		        char c = input.charAt(i);
+		        if (Character.isDigit(c)) {
+		            int num = Character.getNumericValue(c);
+		            System.out.print(num);
+		        }
+		    }
+		    scan.close();
 		
 	}	//main e
 		
