b7a260cbb2177ba71f92471da85df89087c62506
koeunsi <koeunsi@naver.com>
2023-02-09 17:45:42 +0900

    while탈출 시 조건을 걸어줘야함

--- MagicSquare/src/StrParse/Str.java
+++ MagicSquare/src/StrParse/Str.java
@@ -6,19 +6,29 @@ public class Str {
 	
 	public static void main(String[] args) {
 	    Scanner scanner = new Scanner(System.in);
-	    int a = 0;
+	    String a = "0";
+	    
+	    
 	    while(true){
 	    	System.out.println("출력할 수를 입력하세요: ");
-		    String input = scanner.nextLine();
-		    for (int i = 0; i < input.length(); i++) {
-		        char c = input.charAt(i);
-			        if (Character.isDigit(c)) {
-			            int num = Character.getNumericValue(c);
+		    String input = scanner.next();
+		    //System.out.println("/" + input + "/");
+		    if(input.equals(a)){
+		    	System.out.println("끝!!!!");
+		    	break;
+		    }
+		    
+		    
+		    for (int i = 0; i < input.length(); i++){
+		        char c = input.charAt(i);	//int -> char 변환
+			        if (Character.isDigit(c)) {	//문자 숫자 판별
+//			        	int num = Integer.parseInt(String.valueOf(input));
+			            int num = Character.getNumericValue(c);	//shar -> int 변환
 			            //String s = String.format("%3d",num);	//띄어쓰기 조절
-			            System.out.println(String.format("입력한 수 : %3d",num));
+			            System.out.print(String.format("입력한 수 : %3d",num));
 			        }	//if e
 		    }
-//		    return a;
+//		    break ;
 //		    scanner.close(); 가비지 컬렉터가 다 쓴 데이터를 가져감
 	    }	//while e
 	}	//main e
