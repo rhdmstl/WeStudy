2af025dcc80c04e6b2c5fad2dfe349435917ae79
koeunsi <koeunsi@naver.com>
2023-02-13 17:53:46 +0900

    문자열 파싱 완료

--- MagicSquare/src/StrParse/Parse.java
+++ MagicSquare/src/StrParse/Parse.java
@@ -3,56 +3,36 @@ package StrParse;
 import java.util.Scanner;
 
 public class Parse {
-
-	Scanner scanner = new Scanner(System.in);
 	
-	public void test(){
-		while(loop(null)){
-			System.out.println("입력한 수");
-		}
-	}
-	
-	//마방진 칸 수 입력받기
-	private int getInput(){	
+	public static void main(String[] args) {
 		
-		int a = 0 ;		//마방진을 끝내기
-		
-		while(true){	//입력 안내하기
-			System.out.println("입력한 수 =>");
-			
-			String input = scanner.next();	//마방진 칸 수_String으로 먼저 받아서 문자열 오류 잡기
-			
-			try {	
-				a = Integer.parseInt(input);	//문자로 받은 값을 숫자로 파싱해주기
-				break;	//나가기
-			} catch (Exception e) {
-				//e.printStackTrace();	이게 있으면 문자열 받았을때 에러안내 나옴
-			}
-		}
-		return a;	//마방진 끝
-	}
-	
-	public boolean loop(String input){
-		
-		int a = getInput();	//입력받는 메소드 홀수
-		
-		if( a == 0) return false;	//0 입력시 메소드 종료
-		
-		while(true){
-			for(int i =0 ; i <input.length() ; i++){
-				char c = input.charAt(i);
-				if(Character.isDigit(c)){
-					int number = Character.getNumericValue(c);
-					//String s = String.format("%3d",number);	//띄어쓰기 조절
-					System.out.print("입력한 수"+number);
-				}	//if e
-			
-			}	//for e
-			
-			return true;
-			
-		}	//while e
-		
-	}	//loop e
+	    Scanner scanner = new Scanner(System.in);
+	    String a = "0";	//0을 입력하면 탈출
+	    
+	    while(true){
+	    	System.out.println("출력할 수를 입력하세요: ");
+		    String input = scanner.next();
+		    //System.out.println("/" + input + "/");
+		    if(input.equals(a)){
+		    	System.out.println("끝!!!!");
+		    	break;
+		    }
+		    
+		    System.out.print("입력한 수 : ");
+		    for (int i = 0; i < input.length(); i++){
+		        char c = input.charAt(i);	//int -> char 변환
+			        if (Character.isDigit(c)) {	//문자 숫자 판별
+//			        	int num = Integer.parseInt(String.valueOf(input));
+			            int num = Character.getNumericValue(c);	//shar -> int 변환
+			            //String s = String.format("%3d",num);	//띄어쓰기 조절
+			            System.out.print(num);
+			        }	//if e
+		    }
+		    System.out.println();
+	    }	//while e
+	    
+	}	//main e
 		
 }	
+
+//		    scanner.close(); 가비지 컬렉터가 다 쓴 데이터를 가져감
--- MagicSquare/src/StrParse/Str.java
+++ MagicSquare/src/StrParse/Str.java
@@ -4,32 +4,14 @@ import java.util.Scanner;
 
 public class Str {
 	
+//	public int mCnt = 0;	메모리 할당 된 선언 예시들
+//	public Parse par = new Parse();	
+	
 	public static void main(String[] args) {
-	    Scanner scanner = new Scanner(System.in);
-	    String a = "0";
-	    
-	    
-	    while(true){
-	    	System.out.println("출력할 수를 입력하세요: ");
-		    String input = scanner.next();
-		    //System.out.println("/" + input + "/");
-		    if(input.equals(a)){
-		    	System.out.println("끝!!!!");
-		    	break;
-		    }
-		    
-		    
-		    for (int i = 0; i < input.length(); i++){
-		        char c = input.charAt(i);	//int -> char 변환
-			        if (Character.isDigit(c)) {	//문자 숫자 판별
-//			        	int num = Integer.parseInt(String.valueOf(input));
-			            int num = Character.getNumericValue(c);	//shar -> int 변환
-			            //String s = String.format("%3d",num);	//띄어쓰기 조절
-			            System.out.print(String.format("입력한 수 : %3d",num));
-			        }	//if e
-		    }
-//		    break ;
-//		    scanner.close(); 가비지 컬렉터가 다 쓴 데이터를 가져감
-	    }	//while e
+		//실행 클래스 호출
+		Parse Parse = new Parse();
+		Parse.loop();
+		
 	}	//main e
+	
 }
